e34d9aa731e8d5e9bfeed427cbc1afb91dea52bc
Sinué Flores <16889715+Sinnup@users.noreply.github.com>
2026-05-25 16:28:53 -0600

    fix(nav): resolve deprecated getParcelable call in NavTypeUtils

--- core/src/main/java/com/espert/dogedex/core/navigation/NavTypeUtils.kt
+++ core/src/main/java/com/espert/dogedex/core/navigation/NavTypeUtils.kt
@@ -1,18 +1,15 @@
 package com.espert.dogedex.core.navigation
 
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
-import androidx.annotation.RequiresApi
+import androidx.core.os.BundleCompat
 import androidx.navigation.NavType
 import com.espert.dogedex.core.model.Dog
 import kotlinx.serialization.json.Json
 
 val DogType = object : NavType<Dog>(isNullableAllowed = false) {
-    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun get(bundle: Bundle, key: String): Dog? {
-
-        return if ()bundle.getParcelable(key, Dog::class.java)
+        return BundleCompat.getParcelable(bundle, key, Dog::class.java)
     }
 
     override fun parseValue(value: String): Dog {
